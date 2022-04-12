95b62e45ea8376a7d9b01a48d447c62f3a0719b8
sivaprakash1234 <103481257+sivaprakash1234@users.noreply.github.com>
2022-04-12 13:59:44 +0530

    New file added successfully

---
+++ siva/src/siva/hello.java
@@ -0,0 +1,17 @@
+package siva;
+ 
+public class hello {
+
+	
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+     char a= 's';
+     int b=10;
+      String aa = "sivaprakash";
+      System.out.println(aa);
+     
+     
+	}
+
+}
